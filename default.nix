af6d21c9568c470f53496883b2ace87d467c907f
Jongkook Choi <jongkook90@hotmail.com>
2025-11-24 19:08:06 +0900

    fixed INSTALL.txt

--- default.nix
+++ default.nix
@@ -1,19 +1,13 @@
 let
   pkgs = import <nixpkgs> { };
   whispercli = pkgs.callPackage ./whisper-cpp/package.nix {};
-  respeaker-record = pkgs.rustPlatform.buildRustPackage rec {
+in 
+  pkgs.rustPlatform.buildRustPackage rec {
     pname = "respeaker-record";
     version = "0.2.0";
     cargoLock.lockFile = ./Cargo.lock;
     src = pkgs.lib.cleanSource ./.;
-    nativeBuildInputs = with pkgs; [ pkg-config ];
+    nativeBuildInputs = with pkgs; [ pkg-config whispercli ];
     buildInputs = with pkgs; [
       cargo rustc pkg-config alsa-lib libusb1 ];
-  };
-in 
-pkgs.buildEnv {
-  name = "respeaker-env";
-  paths = [
-    respeaker-record whispercli
-  ];
-}
+  }
