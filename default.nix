ce7e61e426b1a6e9ded2cd48f3f074da9013cbdf
Jongkook Choi <jongkook90@hotmail.com>
2025-11-24 19:20:03 +0900

    fix default.nix

--- default.nix
+++ default.nix
@@ -1,12 +1,10 @@
-let
-  pkgs = import <nixpkgs> { };
-in 
-  pkgs.rustPlatform.buildRustPackage rec {
-    pname = "respeaker-record";
-    version = "0.2.0";
-    cargoLock.lockFile = ./Cargo.lock;
-    src = pkgs.lib.cleanSource ./.;
-    nativeBuildInputs = with pkgs; [ pkg-config ];
-    buildInputs = with pkgs; [
-      cargo rustc pkg-config alsa-lib libusb1 ];
-  }
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "respeaker-record";
+  version = "0.2.0";
+  cargoLock.lockFile = ./Cargo.lock;
+  src = pkgs.lib.cleanSource ./.;
+  nativeBuildInputs = with pkgs; [ pkg-config ];
+  buildInputs = with pkgs; [
+    cargo rustc pkg-config alsa-lib libusb1 ];
+}
